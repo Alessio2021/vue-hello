8bc6bfd62fa05e42a09a1b073ca330976a4b74c7
Alessio2021 <alessiobut1911@gmail.com>
2021-12-15 15:23:30 +0100

    bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,12 +1,22 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+
 
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!',
-        image: 'https://picsum.photos/200/300'
+        message: 'Hello Monkey!',
+        image: 'https://picsum.photos/200/300',
+        text: 'Write on it!',
+        image2: 'https://picsum.photos/id/1/200/300',
+        number: 5
     },
+    methods: {
+        myFunction: function () {
+            return this.number += 1
+        }
+    }
 
 })
